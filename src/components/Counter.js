014731a0784b1c7352eb1d61d39d4c682d80054b
snrntpa2z@naver.com <snrntpa2z@naver.com>
2019-04-28 22:55:55 +0900

    redux-hooks : useSelector, useActions

---
+++ src/components/Counter.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Counter = ({ onIncrease, onDecrease, number }) => {
+  return (
+    <div>
+      <h1>{number}</h1>
+      <div>
+        <button onClick={onIncrease}>+</button>
+        <button onClick={onDecrease}>-</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
